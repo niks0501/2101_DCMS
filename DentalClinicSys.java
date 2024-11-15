c5aebf94a2f0ba32c1919a8c88afbfcae9d39f7b
unknown <nikkocausapin61@gmail.com>
2024-11-15 14:39:10 +0800

    Here is the loading, login and the unmodified java files

---
+++ DentalClinicSys.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+
+
+/**
+ *
+ * @author Nikko
+ */
+public class DentalClinicSys {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
