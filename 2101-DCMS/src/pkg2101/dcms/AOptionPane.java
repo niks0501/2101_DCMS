c760eb9cdd8fcc44e1090d094205215f10706d0c
CHRISTIAN ANCHOREZ <anchorezchristian2503@gmail.com>
2024-11-19 12:26:59 +0800

    patient, treatment, payment UI

---
+++ 2101-DCMS/src/pkg2101/dcms/AOptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg2101.dcms;
+
+/**
+ *
+ * @author christian
+ */
+class AOptionPane {
+
+    static void ShowMessageDialog(Object object, String please_Enter_Username) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
---
+++ 2101-DCMS/src/pkg2101/dcms/DCMS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg2101.dcms;
+
+/**
+ *
+ * @author christian
+ */
+public class DCMS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
